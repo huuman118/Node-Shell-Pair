08fd19bca02dac22078f5b10a539d30b3f7057b0
Huu Nguyen <huu.nguyen@gmail.com>
2018-04-24 12:46:05 -0400

    added ls and pwd

--- bash.js
+++ bash.js
@@ -1,15 +1,17 @@
+const pwd = require('./pwd.js').pwd
+const ls = require('./ls')
+// const fs = require('fs');
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
   if(cmd === "pwd") {
-    process.stdout.write(process.cwd());
-    process.stdout.write('\nprompt > ')
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
   } else {
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ')
   }
-
-
 });
